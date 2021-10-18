9c96b9c4a49cc542b31ef16193fb5724d53196d4
Jana <90056190+nesti13@users.noreply.github.com>
2021-10-18 11:59:44 +0300

    Add files via upload

---
+++ лаба12/Lab12/ex/ex.cpp
@@ -0,0 +1,35 @@
+﻿#include <windows.h>
+#include <tchar.h> 
+
+DWORD Number; 
+
+DWORD WINAPI Reader(LPVOID lpparameter) 
+{
+	_tprintf_s(TEXT("Counter= %d\n"), Number); 
+	return 0;
+}
+
+DWORD WINAPI Writer(LPVOID lpParameter) { 
+	unsigned int counter = 0;
+	DWORD dwMyThreadID1;
+
+	while (counter != 10) {
+		Number = counter; 
+		HANDLE hMyHandle2 = CreateThread(NULL, 0, Reader, 0, 0, &dwMyThreadID1);
+		Sleep(300); 
+		TerminateThread(hMyHandle2, 0); 
+		counter++; 
+	}
+
+	return EXIT_SUCCESS;
+}
+
+INT _tmain(INT argc, _TCHAR* argv[]) {
+	DWORD dwMyThreadID;
+	HANDLE hMyHandle = CreateThread(NULL, 0, Writer, 0, 0, &dwMyThreadID);
+	Sleep(5000);
+	_tprintf_s(TEXT("Main Process end\n"));
+	TerminateThread(hMyHandle, 0);
+	_tsystem(TEXT("pause"));
+	return EXIT_SUCCESS;
+}
---
+++ лаба12/Lab12/ex1/ex1.cpp
@@ -0,0 +1,43 @@
+﻿#include <windows.h>
+#include "string.h"
+#include <iostream>
+using namespace std;
+
+DWORD WINAPI myThread_1(LPVOID lpParameter)
+{
+	unsigned int numb1 = 0;
+	while (numb1 < 60)
+	{
+		Sleep(500);
+		numb1++;
+		cout << numb1 << endl;
+	}
+	return 0;
+}
+
+DWORD WINAPI myThread_2(LPVOID lpParameter)
+{
+	unsigned int numb2 = 100;
+	while (numb2 > 0)
+	{
+		Sleep(500);
+		numb2--;
+		cout << numb2 << endl;
+	}
+	return 0;
+}
+
+int main()
+{
+	unsigned int myCounter = 0;
+	DWORD myThreadID;
+	HANDLE myHandle1 = CreateThread(0, 0, myThread_1, &myCounter, 0, &myThreadID);
+	HANDLE myHandle2 = CreateThread(0, 0, myThread_2, &myCounter, 0, &myThreadID);
+	Sleep(20000);
+	printf("\n The end");
+	TerminateThread(myHandle1, 0);
+	TerminateThread(myHandle2, 0);
+	getchar();
+
+	return 0;
+}
